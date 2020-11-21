d00a2f45d421d0e76afaa4f3530bbbf075d082de
Junichi Ito <jit@sonicgarden.jp>
2020-11-22 08:51:59 +0900

    seeds.rbの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,36 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+print '開発環境のデータをすべて削除して初期データを投入します。よろしいですか？[Y/n]: ' # rubocop:disable Rails/Output
+unless $stdin.gets.chomp == 'Y'
+  puts '中止しました。' # rubocop:disable Rails/Output
+  return
+end
+
+def picture_file(name)
+  File.open(Rails.root.join("db/seeds/#{name}"))
+end
+
+Book.destroy_all
+
+Book.create!(
+  title: 'Ruby超入門',
+  memo: 'Rubyの文法の基本をやさしくていねいに解説しています。',
+  author: '五十嵐 邦明',
+  picture: picture_file('cho-nyumon.jpg')
+)
+
+Book.create!(
+  title: 'チェリー本',
+  memo: 'プログラミング経験者のためのRuby入門書です。',
+  author: '伊藤 淳一',
+  picture: picture_file('cherry-book.jpg')
+)
+
+Book.create!(
+  title: '楽々ERDレッスン',
+  memo: '実在する帳票から本当に使えるテーブル設計を導く画期的な本！',
+  author: '羽生 章洋',
+  picture: picture_file('erd.jpg')
+)
+
+puts '初期データの投入が完了しました。' # rubocop:disable Rails/Output
