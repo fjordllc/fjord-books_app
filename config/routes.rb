7ac408c94adc2df9fe842bd1f374bc2999978143
Junichi Ito <jit@sonicgarden.jp>
2020-11-22 16:43:46 +0900

    パスワードなしでアカウント変更ができるように

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: "users/registrations" }
   root to: 'books#index'
   resources :books
   resources :users, only: %i(index show)
