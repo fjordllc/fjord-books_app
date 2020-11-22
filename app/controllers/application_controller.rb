3b5b389fcf908485e43ace5927793c089b1b4568
Junichi Ito <jit@sonicgarden.jp>
2020-11-22 09:51:09 +0900

    ログイン・ログアウトの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource_or_scope)
+    books_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 end
