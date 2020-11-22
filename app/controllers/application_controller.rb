81c496f9a5d2ca26fc585d7b05a040316f483782
Junichi Ito <jit@sonicgarden.jp>
2020-11-22 10:43:19 +0900

    自分のプロフィールを編集可能に

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,17 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    keys = [:name, :postal_code, :address, :self_introduction]
+    devise_parameter_sanitizer.permit(:sign_up, keys: keys)
+    devise_parameter_sanitizer.permit(:account_update, keys: keys)
+  end
+
+  private
 
   def after_sign_in_path_for(resource_or_scope)
     books_path
@@ -9,5 +20,9 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
+  end
+
+  def signed_in_root_path(resource_or_scope)
+    user_path(current_user)
   end
 end
