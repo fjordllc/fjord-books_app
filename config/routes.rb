4dbde99d7b2ed6687b222a6ef42cdfd2973ffb91
miso160 <hotmisosoup160@gmail.com>
2020-12-19 19:46:41 +0900

    ユーザー一覧と詳細表示の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   root to: 'books#index'
   devise_for :users
   resources :books
+  resources :users, :only => [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
