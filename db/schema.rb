5e75115e0ac12406d8f1d780e9dffc5f0950da31
Junichi Ito <jit@sonicgarden.jp>
2020-12-27 14:49:15 +0900

    rails db:migrateを実行

--- db/schema.rb
+++ db/schema.rb
@@ -50,9 +50,9 @@ ActiveRecord::Schema.define(version: 2020_12_27_052521) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.string "commentable_type", null: false
-    t.integer "commentable_id", null: false
+    t.bigint "commentable_id", null: false
     t.string "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -70,7 +70,7 @@ ActiveRecord::Schema.define(version: 2020_12_27_052521) do
   end
 
   create_table "reports", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.string "title", null: false
     t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
