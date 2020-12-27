32bb8819d2f7ebd6b5c1113f4c83218ede0dabad
Junichi Ito <jit@sonicgarden.jp>
2020-12-27 10:20:51 +0900

    日報のテストを追加

---
+++ test/system/reports_test.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require 'application_system_test_case'
+
+class ReportsTest < ApplicationSystemTestCase
+  setup do
+    sign_in
+  end
+
+  test '日報のCRUD' do
+    # 日報の登録
+    click_link '日報'
+    click_link '新規作成'
+    fill_in 'タイトル', with: 'はじめての日報'
+    fill_in '内容', with: 'はじめまして。よろしくお願いします。'
+    click_button '登録する'
+
+    assert_text '日報が作成されました。'
+    assert_text 'はじめての日報'
+    assert_text 'はじめまして。よろしくお願いします。'
+
+    # 日報の編集
+    click_link '編集'
+    fill_in 'タイトル', with: 'My first report'
+    fill_in '内容', with: 'Hello, everyone!'
+    click_button '更新する'
+    assert_text '日報が更新されました。'
+    assert_text 'My first report'
+    assert_text 'Hello, everyone!'
+
+    # コメント登録
+    fill_in 'comment[content]', with: 'コメントお待ちしています！'
+    click_button 'コメントする'
+    assert_text 'コメントが投稿されました。'
+    assert_text 'コメントお待ちしています！'
+
+    # 日報の一覧と日報の削除
+    click_link '戻る'
+    assert_css 'h1', text: '日報'
+    within 'table' do
+      assert_text 'My first report'
+      click_link '削除'
+      accept_confirm
+    end
+    assert_text '日報が削除されました。'
+    within 'table' do
+      assert_no_text 'My first report'
+    end
+  end
+end
