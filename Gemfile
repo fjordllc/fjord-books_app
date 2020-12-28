359ecc252d22dd42eaba57b26d8b8eee259b84dd
MakiIchikawa <punkfolkfolk@gmail.com>
2020-12-28 11:12:28 +0900

    paginate the booksapp

--- Gemfile
+++ Gemfile
@@ -57,3 +57,5 @@ end
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
 gem 'carrierwave'
+
+gem 'kaminari'
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.all
+    @books = Book.page(params[:page]).order(:id).per(2)
   end
 
   # GET /books/1
