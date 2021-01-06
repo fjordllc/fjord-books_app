d0a44bb2f7d2335a80001624efb9856b88a1a397
kanekodaichi <knkdic.bfc@gmail.com>
2021-01-06 23:51:06 +0900

    Reportモデルを作成

---
+++ db/migrate/20210106140810_create_reports.rb
@@ -0,0 +1,11 @@
+class CreateReports < ActiveRecord::Migration[6.1]
+  def change
+    create_table :reports do |t|
+      t.string :title
+      t.text :content
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
