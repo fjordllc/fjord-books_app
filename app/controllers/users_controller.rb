0644b1809fdd9e8dc673fe65a6e3407f8b34925a
betachelsea <beta.chelsea@gmail.com>
2021-01-10 18:23:09 +0900

    ActiveStorageのN+1問題に対応

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@
 
 class UsersController < ApplicationController
   def index
-    @users = User.order(:id).page(params[:page])
+    @users = User.with_attached_avatar.order(:id).page(params[:page])
   end
 
   def show
