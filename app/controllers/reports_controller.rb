0611682a4258b83cebfac5e2fead9e940b176f46
kanekodaichi <knkdic.bfc@gmail.com>
2021-01-12 20:11:47 +0900

    不要なjson箇所&ファイルの削除

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -30,10 +30,8 @@ class ReportsController < ApplicationController
     respond_to do |format|
       if @report.save
         format.html { redirect_to @report, notice: 'Report was successfully created.' }
-        format.json { render :show, status: :created, location: @report }
       else
         format.html { render :new }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -44,10 +42,8 @@ class ReportsController < ApplicationController
     respond_to do |format|
       if @report.update(report_params)
         format.html { redirect_to @report, notice: 'Report was successfully updated.' }
-        format.json { render :show, status: :ok, location: @report }
       else
         format.html { render :edit }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -58,7 +54,6 @@ class ReportsController < ApplicationController
     @report.destroy
     respond_to do |format|
       format.html { redirect_to reports_url, notice: 'Report was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
--- app/views/reports/_report.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! report, :id, :title, :content, :user_id, :created_at, :updated_at
-json.url report_url(report, format: :json)
