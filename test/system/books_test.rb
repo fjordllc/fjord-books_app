af44b5e694fc82349d2747b639c660e6e5b2dc3f
SeijiNumata <seizi.numata@gmail.com>
2021-01-16 23:57:00 +0900

    systemtestできた

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -6,46 +6,22 @@ class BooksTest < ApplicationSystemTestCase
   setup do
     @book = books(:one)
     visit root_url
-    fill_in 'Eメール', with: 'alice@example.com'
-    fill_in 'パスワード', with: 'password'
+    fill_in 'Eメール',with:'alice@example.com'
+    fill_in 'パスワード',with:'password'
     click_button 'ログイン'
   end
 
-  test 'visiting the index' do
-    visit books_url
-    assert_selector 'h1', text: 'Books'
+  test 'visiting the report index' do
+    visit reports_url
+    assert_selector 'h1', text: '日報'
   end
 
-  test 'creating a Book' do
-    visit books_url
-    click_on 'New Book'
-
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Create Book'
-
-    assert_text 'Book was successfully created'
-    click_on 'Back'
+  test 'creating a new book' do
+    visit reports_url
+    click_on '新規作成'
+    fill_in 'タイトル', with: "タイトル"
+    fill_in '内容', with: "内容"
+    click_on '登録する'
   end
 
-  test 'updating a Book' do
-    visit books_url
-    click_on 'Edit', match: :first
-
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Update Book'
-
-    assert_text 'Book was successfully updated'
-    click_on 'Back'
-  end
-
-  test 'destroying a Book' do
-    visit books_url
-    page.accept_confirm do
-      click_on 'Destroy', match: :first
-    end
-
-    assert_text 'Book was successfully destroyed'
-  end
 end
