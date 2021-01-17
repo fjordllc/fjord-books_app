e497c9ac1ec825b3db3a91d8e7d386576d216445
Junichi Ito <jit@sonicgarden.jp>
2021-01-17 16:45:25 +0900

    rails db:reset時のエラー抑制とパフォーマンス向上 https://bootcamp.fjord.jp/questions/779#answer_2262

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,10 @@
 
 require 'open-uri'
 
+# queue_adapterを変更している理由とtransactionを使っている理由は下記URLを参照
+# https://bootcamp.fjord.jp/questions/779#answer_2262
+ActiveStorage::AnalyzeJob.queue_adapter = :inline
+
 print '開発環境のデータをすべて削除して初期データを投入します。よろしいですか？[Y/n]: ' # rubocop:disable Rails/Output
 unless $stdin.gets.chomp == 'Y'
   puts '中止しました。' # rubocop:disable Rails/Output
@@ -16,48 +20,55 @@ puts '実行中です。しばらくお待ちください...' # rubocop:disable 
 
 Book.destroy_all
 
-Book.create!(
-  title: 'Ruby超入門',
-  memo: 'Rubyの文法の基本をやさしくていねいに解説しています。',
-  author: '五十嵐 邦明',
-  picture: picture_file('cho-nyumon.jpg')
-)
-
-Book.create!(
-  title: 'チェリー本',
-  memo: 'プログラミング経験者のためのRuby入門書です。',
-  author: '伊藤 淳一',
-  picture: picture_file('cherry-book.jpg')
-)
-
-Book.create!(
-  title: '楽々ERDレッスン',
-  memo: '実在する帳票から本当に使えるテーブル設計を導く画期的な本！',
-  author: '羽生 章洋',
-  picture: picture_file('erd.jpg')
-)
-
-50.times do
+Book.transaction do
   Book.create!(
-    title: Faker::Book.title,
-    memo: Faker::Book.genre,
-    author: Faker::Book.author,
-    picture: picture_file('no-image.png')
+    title: 'Ruby超入門',
+    memo: 'Rubyの文法の基本をやさしくていねいに解説しています。',
+    author: '五十嵐 邦明',
+    picture: picture_file('cho-nyumon.jpg')
   )
+
+  Book.create!(
+    title: 'チェリー本',
+    memo: 'プログラミング経験者のためのRuby入門書です。',
+    author: '伊藤 淳一',
+    picture: picture_file('cherry-book.jpg')
+  )
+
+  Book.create!(
+    title: '楽々ERDレッスン',
+    memo: '実在する帳票から本当に使えるテーブル設計を導く画期的な本！',
+    author: '羽生 章洋',
+    picture: picture_file('erd.jpg')
+  )
+
+  50.times do
+    Book.create!(
+      title: Faker::Book.title,
+      memo: Faker::Book.genre,
+      author: Faker::Book.author,
+      picture: picture_file('no-image.png')
+    )
+  end
 end
 
 User.destroy_all
 
-50.times do |n|
-  name = Faker::Name.name
-  user = User.create!(
-    email: "sample-#{n}@example.com",
-    password: 'password',
-    name: name,
-    postal_code: "123-#{n.to_s.rjust(4, '0')}",
-    address: Faker::Address.full_address,
-    self_introduction: "こんにちは、#{name}です。"
-  )
+User.transaction do
+  50.times do |n|
+    name = Faker::Name.name
+    User.create!(
+      email: "sample-#{n}@example.com",
+      password: 'password',
+      name: name,
+      postal_code: "123-#{n.to_s.rjust(4, '0')}",
+      address: Faker::Address.full_address,
+      self_introduction: "こんにちは、#{name}です。"
+    )
+  end
+end
+
+User.order(:id).each do |user|
   image_url = Faker::Avatar.image(slug: user.email, size: '150x150')
   user.avatar.attach(io: URI.parse(image_url).open, filename: 'avatar.png')
 end
