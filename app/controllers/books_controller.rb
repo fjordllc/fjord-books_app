6bd57a071b00b8332da7e8fd75ad07ed34e9d7f2
siso <siso25@zoho.com>
2021-03-01 22:52:46 +0900

    kaminariを使ってページング処理を実装
    kaminariのgemをインストール。
    ページング処理を実装し、ページネーションを日本語化。

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,8 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.all
+    # @books = Book.all
+    @books = Book.order(:id).page(params[:page]).per(1)
   end
 
   # GET /books/1
