8af28e9d7f377203f30c72e44c3f2912c9029e91
yamadaab <s.yamada.1125@gmail.com>
2021-03-02 19:43:28 +0900

    add system_test

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -5,43 +5,48 @@ require 'application_system_test_case'
 class BooksTest < ApplicationSystemTestCase
   setup do
     @book = books(:one)
+    visit root_url
+    fill_in 'Eメール', with: 'alice@example.com'
+    fill_in 'パスワード', with: 'password'
+    click_button 'ログイン'
   end
 
   test 'visiting the index' do
     visit books_url
-    assert_selector 'h1', text: 'Books'
+    assert_selector 'h1', text: '本'
   end
 
   test 'creating a Book' do
     visit books_url
-    click_on 'New Book'
+    click_on '新規作成'
 
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Create Book'
+    fill_in 'タイトル', with: @book.title
+    fill_in 'メモ', with: @book.memo
+    fill_in '著者', with: @book.author
+    click_on '登録する'
 
-    assert_text 'Book was successfully created'
-    click_on 'Back'
+    assert_text '本が作成されました。'
+    click_on '戻る'
   end
 
   test 'updating a Book' do
     visit books_url
-    click_on 'Edit', match: :first
+    click_link '編集', match: :prefer_exact
 
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Update Book'
+    fill_in 'タイトル', with: @book.title
+    fill_in 'メモ', with: @book.memo
+    click_on '更新する'
 
-    assert_text 'Book was successfully updated'
-    click_on 'Back'
+    assert_text '本が更新されました。'
+    click_on '戻る'
   end
 
   test 'destroying a Book' do
     visit books_url
     page.accept_confirm do
-      click_on 'Destroy', match: :first
+      click_on '削除', match: :first
     end
 
-    assert_text 'Book was successfully destroyed'
+    assert_text '本が削除されました。'
   end
 end
