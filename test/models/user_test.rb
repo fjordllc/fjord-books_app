1b92dd1cf8d20918706d7106c557b188edcc6a07
yamadaab <s.yamada.1125@gmail.com>
2021-03-02 15:11:43 +0900

    add user_test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,22 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#name_or_email" do
+    user = User.new(email: "foo@example.com", name: "")
+    assert_equal "foo@example.com", user.name_or_email
+
+    user.name = "Foo Bar"
+    assert_equal "Foo Bar", user.name_or_email
+  end
+
+  test "#follow_and_unfollow" do
+    me = User.create!(email: "me@example.com", password: "password")
+    she = User.create!(email: "she@example.com", password: "password")
+
+    assert_not me.following?(she)
+    me.follow(she)
+    assert me.following?(she)
+    assert me.unfollow(she)
+    assert_not me.following?(she)
+  end
 end
