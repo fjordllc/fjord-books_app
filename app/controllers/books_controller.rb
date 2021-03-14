104c42685102034e96bb196037276c28a50fb1d0
user <aax.chiri@gmail.com>
2021-03-14 10:38:55 +0900

    日本語と英語に対応

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,7 +28,7 @@ class BooksController < ApplicationController
 
     respond_to do |format|
       if @book.save
-        format.html { redirect_to @book, notice: 'Book was successfully created.' }
+        format.html { redirect_to @book, notice: t(:suc_msg) }
         format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new }
@@ -42,11 +42,11 @@ class BooksController < ApplicationController
   def update
     respond_to do |format|
       if @book.update(book_params)
-        format.html { redirect_to @book, notice: 'Book was successfully updated.' }
+        format.html { redirect_to @book, notice: t(:update_suc_msg) }
         format.json { render :show, status: :ok, location: @book }
       else
         format.html { render :edit }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
+        format.json { render json: @book.errors, status: t(:unprocessable_entity) }
       end
     end
   end
@@ -56,7 +56,7 @@ class BooksController < ApplicationController
   def destroy
     @book.destroy
     respond_to do |format|
-      format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
+      format.html { redirect_to books_url, notice: t(:del_suc_msg) }
       format.json { head :no_content }
     end
   end
