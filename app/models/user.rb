3a8bf9328ac167797cd9a262d0db3660266e35a6
Saki-htr <saki.hattori0116@gmail.com>
2021-04-17 11:53:14 +0900

    omniauth_callbacl_controllerをカスタマイズして、GitHub認証を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[github]
-end
 
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20] # 任意の20文字のパスワードを作成
+    end
+  end
+end
