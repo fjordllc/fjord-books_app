e9f7493203d1b8c8cdba82957ea7adcba0e1d222
konaga-k <priests2020@gmail.com>
2021-06-13 16:40:34 +0900

    Add a model test of Report, a model test of Book, and a systen test of Report

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,21 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @report = reports(:one)
+    @alice = users(:alice)
+    @bob = users(:bob)
+  end
+
+  test '#editable? when the target_user is equal to an author of the report' do
+    assert(@report.editable?(@alice))
+  end
+
+  test '#editable? when the target_user is not equal to an author of the report' do
+    assert_not(@report.editable?(@bob))
+  end
+
+  test 'created_on' do
+    assert_equal(@report.created_at.to_date, @report.created_on)
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,57 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @alice = users(:alice)
+    @bob = users(:bob)
+    @carol = users(:carol)
+  end
+
+  test '#following? when Alice is following Bob' do
+    assert(@alice.following?(@bob))
+  end
+
+  test '#following? when Alice is not following Carol' do
+    assert_not(@alice.following?(@carol))
+  end
+
+  test '#followed_by? when Bob is following Alice' do
+    assert(@bob.followed_by?(@alice))
+  end
+
+  test '#followed_by? when Carol is following Alice' do
+    assert_not(@carol.followed_by?(@alice))
+  end
+
+  test '#follow when Alice is not following Carol' do
+    assert_not_includes(@alice.followings, @carol)
+    @alice.follow(@carol)
+    assert_includes(@alice.followings, @carol)
+  end
+
+  test '#follow when Alice is not following Bob' do
+    assert_includes(@alice.followings, @bob)
+    @alice.follow(@bob)
+    assert_includes(@alice.followings, @bob)
+  end
+
+  test '#unfollow when Alice is following Bob' do
+    assert_includes(@alice.followings, @bob)
+    @alice.unfollow(@bob)
+    assert_not_includes(@alice.followings, @bob)
+  end
+
+  test '#unfollow when Alice is not following Carol' do
+    assert_not_includes(@alice.followings, @carol)
+    @alice.unfollow(@carol)
+    assert_not_includes(@alice.followings, @carol)
+  end
+
+  test 'name when Alice has set her name' do
+    assert_equal('Alice', @alice.name_or_email)
+  end
+
+  test 'name when Bob has not set his name' do
+    assert_equal('bob@example.com', @bob.name_or_email)
+  end
 end
