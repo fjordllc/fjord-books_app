217536221fe696c543909f8e1ab797dcf4695e1f
Miki Watanabe <watami03@gmail.com>
2021-07-17 18:14:34 +0200

    add: statistics and follow form

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,16 @@ class User < ApplicationRecord
 
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :books
   resources :users, only: %i(index show)
   resources :relationships, only: %i[create destroy]
+  resources :relationships, only: %i[create, destroy]
 end
