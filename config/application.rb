dec441c181fe824a2b7c980c6d0c9a6b1f912dbc
saeyama <camper_green_yellow_21@yahoo.co.jp>
2021-10-25 17:51:48 +0900

    各viewのページに日本語設定

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module BooksApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
   end
 end
