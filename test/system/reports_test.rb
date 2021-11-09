48bc9db591fc367635d46fa70712027a845a31cb
saeyama <camper_green_yellow_21@yahoo.co.jp>
2021-11-09 20:40:06 +0900

    reportsのシステムテストを作成

---
+++ test/system/reports_test.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require 'application_system_test_case'
+
+class ReportsTest < ApplicationSystemTestCase
+  setup do
+    @report = reports(:one)
+
+    visit root_url
+    fill_in 'Eメール', with: 'alice@example.com'
+    fill_in 'パスワード', with: 'password'
+    click_button 'ログイン'
+  end
+
+  test 'visiting the index' do
+    visit reports_url
+    assert_selector 'h1', text: '日報'
+  end
+
+  test 'creating a Book' do
+    visit reports_url
+    click_on '新規作成'
+    fill_in '内容', with: @report.content
+    fill_in 'タイトル', with: @report.title
+    click_on '登録する'
+
+    assert_text '日報が作成されました。'
+    click_on '戻る'
+  end
+
+  test 'updating a Book' do
+    visit reports_url
+    click_on '編集'
+
+    fill_in '内容', with: @report.content
+    fill_in 'タイトル', with: @report.title
+    click_on '更新する'
+
+    assert_text '日報が更新されました。'
+    click_on '戻る'
+  end
+
+  test 'destroying a Book' do
+    visit reports_url
+    page.accept_confirm do
+      click_on '削除', match: :first
+    end
+    assert_text '日報が削除されました。'
+  end
+end
