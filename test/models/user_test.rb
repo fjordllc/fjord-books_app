d439011bdd7c07486533ae62bd8e33f7f2a4620a
saeyama <camper_green_yellow_21@yahoo.co.jp>
2021-11-09 20:06:29 +0900

    Userクラスのモデルテストを作成

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,37 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#name_or_email' do
+    user = User.new(email: 'test1@example.com', name: '')
+    assert_equal 'test1@example.com', user.name_or_email
+
+    user.name = 'test1'
+    assert_equal 'test1', user.name_or_email
+  end
+
+  test '#follow' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    she = User.create!(email: 'she@example.com', password: 'password')
+
+    assert_not me.following?(she)
+    me.follow(she)
+    assert me.following?(she)
+  end
+
+  test '#unfollow' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    she = User.create!(email: 'she@example.com', password: 'password')
+    me.follow(she)
+    assert me.following?(she)
+    me.unfollow(she)
+    assert_not me.following?(she)
+  end
+
+  test '#followed_by?' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    she = User.create!(email: 'she@example.com', password: 'password')
+    assert_not me.following?(she)
+    me.follow(she)
+    assert she.followed_by?(me)
+  end
 end
