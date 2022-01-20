42e1b22e8657f486f021c6da54bc54b1925e2119
rosh-1228 <sho.watanabe1228@gmail.com>
2022-01-20 13:42:17 +0900

    リファクタリング】comment_controllerからset_pathとset_dataメソッドを削除し、concerns/common_moduleのcollect_model_paramメソッドを実実装。comment_controller内でのモデルデータ収集/リクエスト元を判断してidをセット/リダイレクト後のパス設定をcollect_model_paramメソッドで実現できるように設定。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,32 +2,28 @@ class CommentsController < ApplicationController
   include CommonModule
 
   def create
-    commentable_type = request.path.split('/')[1].singularize.capitalize
-
     respond_to do |format|
-      if set_data(commentable_type).comments.create(comment_params).valid?
-        format.html { redirect_to set_path(commentable_type) }
+      if collect_model_param[:set_type].comments.create(comment_params).valid?
+        format.html { redirect_to collect_model_param[:path] }
       else
-        format.html { redirect_to set_path(commentable_type), alert: 'コメントを登録できませんでした。'}
+        format.html { redirect_to collect_model_param[:path], alert: 'コメントを登録できませんでした。'}
       end
     end
   end
 
   def edit
-    commentable_type = request.path.split('/')[1].singularize.capitalize
-    @model = set_data(commentable_type)
+    @model = collect_model_param[:set_type]
     @comment = Comment.find(params[:id])
   end
 
   def update
     comment = Comment.find(params[:id])
-    commentable_type = request.path.split('/')[1].singularize.capitalize
 
     respond_to do |format|
       if comment.update(comment_params)
-        format.html { redirect_to set_path(commentable_type) }
+        format.html { redirect_to collect_model_param[:path] }
       else
-        format.html { redirect_to set_path(commentable_type), alert: 'コメントを更新できませんでした。'}
+        format.html { redirect_to collect_model_param[:path], alert: 'コメントを更新できませんでした。'}
       end
     end
   end
@@ -35,30 +31,12 @@ class CommentsController < ApplicationController
   def destroy
     comment = Comment.find(params[:id])
     comment.destroy
-    commentable_type = request.path.split('/')[1].singularize.capitalize
-    redirect_to set_path(commentable_type), notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
+    redirect_to collect_model_param[:path], notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
   end
 
   private
 
   def comment_params
     params.require(:comment).permit(:comment_content, :commentable_type, :user_id)
-  end
-
-  def set_data(type)
-    if type == 'Book'
-      Book.find(params[:book_id])
-    elsif type == 'Report'
-      Report.find(params[:report_id])
-    end
-  end
-
-  def set_path(commentable_type)
-    path_data = {commentable_type: commentable_type, id: set_book_or_report_id(commentable_type)}
-    if commentable_type == 'Book'
-      book_path(path_data)
-    elsif commentable_type == 'Report'
-      report_path(path_data)
-    end
   end
 end
--- app/controllers/concerns/common_module.rb
+++ app/controllers/concerns/common_module.rb
@@ -14,12 +14,14 @@ module CommonModule
     end
   end
 
-  def collect_model_param(commentable_type)
+  def collect_model_param
     parameters = {}
-    parameters << if commentable_type == 'Book'
-      {params[:book_id]}
+    commentable_type = request.path.split('/')[1].singularize.capitalize
+    path_data = {commentable_type: commentable_type, id: set_book_or_report_id(commentable_type)}
+    parameters = if commentable_type == 'Book'
+      {id: params[:book_id], set_type: Book.find(params[:book_id]), path: book_path(path_data)}
     elsif commentable_type == 'Report'
-      {params[:report_id]}
+      {id: params[:report_id], set_type: Report.find(params[:report_id]), path: report_path(path_data)}
     end
   end
 end
