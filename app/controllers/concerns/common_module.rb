b0cc27d7fc1ca315017db75f1542992b4ba7d8d0
rosh-1228 <sho.watanabe1228@gmail.com>
2022-01-20 14:09:02 +0900

    不要なset_book_or_report_idを削除

--- app/controllers/concerns/common_module.rb
+++ app/controllers/concerns/common_module.rb
@@ -6,22 +6,13 @@ module CommonModule
     User.joins(:comments).preload(:comments).select('comments.*, users.name, users.email')
   end
 
-  def set_book_or_report_id(type)
-    if type == 'Book'
-      params[:book_id]
-    elsif type == 'Report'
-      params[:report_id]
-    end
-  end
-
   def collect_model_param
     parameters = {}
     commentable_type = request.path.split('/')[1].singularize.capitalize
-    path_data = {commentable_type: commentable_type, id: set_book_or_report_id(commentable_type)}
     parameters = if commentable_type == 'Book'
-      {id: params[:book_id], set_type: Book.find(params[:book_id]), path: book_path(path_data)}
+      {id: params[:book_id], set_type: Book.find(params[:book_id]), path: book_path(params[:book_id])}
     elsif commentable_type == 'Report'
-      {id: params[:report_id], set_type: Report.find(params[:report_id]), path: report_path(path_data)}
+      {id: params[:report_id], set_type: Report.find(params[:report_id]), path: report_path(params[:report_id])}
     end
   end
 end
