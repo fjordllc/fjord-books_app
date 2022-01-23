1ad63e8f5b19871fd560d12c7caec9953c2a8b02
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2022-01-23 16:05:33 +0900

    Fix existing controller test for books

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -3,21 +3,30 @@
 require 'test_helper'
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
+  include Warden::Test::Helpers
+
   setup do
+    @user = users(:one)
     @book = books(:one)
   end
 
   test 'should get index' do
+    login_as(@user, scope: :user) #追加
+
     get books_url
     assert_response :success
   end
 
   test 'should get new' do
+    login_as(@user, scope: :user) #追加
+
     get new_book_url
     assert_response :success
   end
 
   test 'should create book' do
+    login_as(@user, scope: :user) #追加
+
     assert_difference('Book.count') do
       post books_url, params: { book: { memo: @book.memo, title: @book.title } }
     end
@@ -26,21 +35,29 @@ class BooksControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should show book' do
+    login_as(@user, scope: :user) #追加
+
     get book_url(@book)
     assert_response :success
   end
 
   test 'should get edit' do
+    login_as(@user, scope: :user) #追加
+
     get edit_book_url(@book)
     assert_response :success
   end
 
   test 'should update book' do
+    login_as(@user, scope: :user) #追加
+
     patch book_url(@book), params: { book: { memo: @book.memo, title: @book.title } }
     assert_redirected_to book_url(@book)
   end
 
   test 'should destroy book' do
+    login_as(@user, scope: :user) #追加
+
     assert_difference('Book.count', -1) do
       delete book_url(@book)
     end
