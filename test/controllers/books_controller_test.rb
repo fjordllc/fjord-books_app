0b16e23bc5e55f9c841f42bb75e6ace2d5cdbbe6
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2022-01-24 15:13:31 +0900

    Pass all existing tests

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -11,21 +11,21 @@ class BooksControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should get index' do
-    login_as(@user, scope: :user) #追加
+    login_as(@user, scope: :user)
 
     get books_url
     assert_response :success
   end
 
   test 'should get new' do
-    login_as(@user, scope: :user) #追加
+    login_as(@user, scope: :user)
 
     get new_book_url
     assert_response :success
   end
 
   test 'should create book' do
-    login_as(@user, scope: :user) #追加
+    login_as(@user, scope: :user)
 
     assert_difference('Book.count') do
       post books_url, params: { book: { memo: @book.memo, title: @book.title } }
@@ -35,28 +35,28 @@ class BooksControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should show book' do
-    login_as(@user, scope: :user) #追加
+    login_as(@user, scope: :user)
 
     get book_url(@book)
     assert_response :success
   end
 
   test 'should get edit' do
-    login_as(@user, scope: :user) #追加
+    login_as(@user, scope: :user)
 
     get edit_book_url(@book)
     assert_response :success
   end
 
   test 'should update book' do
-    login_as(@user, scope: :user) #追加
+    login_as(@user, scope: :user)
 
     patch book_url(@book), params: { book: { memo: @book.memo, title: @book.title } }
     assert_redirected_to book_url(@book)
   end
 
   test 'should destroy book' do
-    login_as(@user, scope: :user) #追加
+    login_as(@user, scope: :user)
 
     assert_difference('Book.count', -1) do
       delete book_url(@book)
--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -3,45 +3,56 @@
 require 'application_system_test_case'
 
 class BooksTest < ApplicationSystemTestCase
+  include Warden::Test::Helpers
+
   setup do
+    @user = users(:one)
     @book = books(:one)
   end
 
   test 'visiting the index' do
+    login_as(@user, scope: :user)
+
     visit books_url
-    assert_selector 'h1', text: 'Books'
+    assert_selector 'h1', text: Book.model_name.human
   end
 
   test 'creating a Book' do
+    login_as(@user, scope: :user)
+
     visit books_url
-    click_on 'New Book'
+    click_on I18n.t('views.common.new')
 
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Create Book'
+    fill_in I18n.t('activerecord.attributes.book.memo'), with: @book.memo
+    fill_in I18n.t('activerecord.attributes.book.title'), with: @book.title
+    click_on I18n.t('helpers.submit.create')
 
-    assert_text 'Book was successfully created'
-    click_on 'Back'
+    assert_text I18n.t('controllers.common.notice_create', name: Book.model_name.human)
+    click_on I18n.t('views.common.back')
   end
 
   test 'updating a Book' do
+    login_as(@user, scope: :user)
+
     visit books_url
-    click_on 'Edit', match: :first
+    click_on I18n.t('views.common.edit'), match: :prefer_exact
 
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Update Book'
+    fill_in I18n.t('activerecord.attributes.book.memo'), with: @book.memo
+    fill_in I18n.t('activerecord.attributes.book.title'), with: @book.title
+    click_on I18n.t('helpers.submit.update')
 
-    assert_text 'Book was successfully updated'
-    click_on 'Back'
+    assert_text I18n.t('controllers.common.notice_update', name: Book.model_name.human)
+    click_on I18n.t('views.common.back')
   end
 
   test 'destroying a Book' do
+    login_as(@user, scope: :user)
+
     visit books_url
     page.accept_confirm do
-      click_on 'Destroy', match: :first
+      click_on  I18n.t('views.common.destroy'), match: :first
     end
 
-    assert_text 'Book was successfully destroyed'
+    assert_text I18n.t('controllers.common.notice_destroy', name: Book.model_name.human)
   end
 end
