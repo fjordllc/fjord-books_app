009d91686831d2b2fb5206df94e1c3c22e5d6e48
yuma-matsui <oshino.iiyo@gmail.com>
2022-05-14 09:25:47 +0900

    モデルの関連付け

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,4 +2,5 @@
 
 class Report < ApplicationRecord
   belongs_to :user
+  has_many :comments, as: :commentable, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :reports, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   has_many :active_relationships, class_name: 'Relationship', foreign_key: :follower_id, dependent: :destroy, inverse_of: :follower
   has_many :followings, through: :active_relationships, source: :following
