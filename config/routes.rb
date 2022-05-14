92e340f230d014498b7b9ff0cec19068cd0fa588
yuma-matsui <oshino.iiyo@gmail.com>
2022-05-14 10:56:13 +0900

    コメント作成用のルーティング定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  resources :reports
+  resources :reports do
+    resources :comments, only: %i[create destroy]
+  end
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
-  resources :books
+  resources :books do
+    resources :comments, only: %i[create destroy]
+  end
   resources :users, only: %i[index show] do
     resource :relationships, only: %i[create destroy]
     scope module: :users do
