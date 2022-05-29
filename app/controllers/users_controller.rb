cbab730070ea4dd69162de443e8a29bfdd88edd9
kekosh <kenta0540@gmail.com>
2022-05-29 13:07:17 +0900

    ユーザー情報詳細画面（show）を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal = true
 
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
   def index
     @users = User.order(:id).page(params[:page])
   end
@@ -8,4 +10,11 @@ class UsersController < ApplicationController
   def show
 
   end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
