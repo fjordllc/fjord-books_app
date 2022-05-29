f7372f2382b211beaeb2d8c637b6b71cc1d69f26
kekosh <kenta0540@gmail.com>
2022-05-29 12:33:33 +0900

    ユーザー一覧ページ(index)追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'user/index'
   devise_for :users, controllers: {registrations: 'user/registrations'}
+  resources :users, only: [:index, :show]
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
