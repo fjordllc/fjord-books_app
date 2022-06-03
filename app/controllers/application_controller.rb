d47adff21e607d2440e819bb333fdad325af4e9d
kekosh <kenta0540@gmail.com>
2022-06-04 07:28:55 +0900

    configure_account_update_paramsをappliationコントローラーからuser/registrationsコントローラーに移動

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[name email postal_code address information])
-  end
 end
--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -51,9 +51,9 @@ class User::RegistrationsController < Devise::RegistrationsController
   # end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  #  end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name email postal_code address information])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
