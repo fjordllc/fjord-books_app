2331d7bbc69ece511a629be482f87c6013900228
kekosh <kenta0540@gmail.com>
2022-06-04 17:19:33 +0900

    rubocopチェック結果反映

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(*)
     books_path
   end
 end
--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -2,7 +2,7 @@
 
 class User::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
-  before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params
 
   # GET /resource/sign_up
   # def new
