984a8727e2cc1aa59eb41a29e00621f2c0445104
kekosh <kenta0540@gmail.com>
2022-06-04 18:23:10 +0900

    gem letter_opener_webを追加インストール パスワード再設定メールの送信とメール経由でのパスワード変更に対応

--- Gemfile
+++ Gemfile
@@ -62,3 +62,7 @@ gem 'kaminari'
 
 gem 'devise'
 gem 'devise-i18n'
+
+group :development do
+  gem 'letter_opener_web', '~>2.0'
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'books#index'
+
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
