717fbf21605222b402005948b5a43b2e203c6b12
doitumura <1993nakayama@gmail.com>
2022-06-23 14:05:12 +0900

    kaminariを使用したpagination機能を追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.all
+    @books = Book.all.order(created_at: :asc).page(params[:page])
   end
 
   # GET /books/1
