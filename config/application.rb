09914d287831832c78faead7312c96e7e43c4070
fujii999 <fujii.24q@gmail.com>
2022-09-07 13:04:33 +0900

    i18nを用いて国際化に対応

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module BooksApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   end
 end
