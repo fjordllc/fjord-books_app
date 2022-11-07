1b3f7db49281e1870e946aeb5f9e827d9a96cca3
Toshiyasu Nonohara <hh0648075@gmail.com>
2022-11-08 06:18:56 +0900

    ハンズオン動画に倣ってbooksのシステムテストを作成

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -5,43 +5,44 @@ require 'application_system_test_case'
 class BooksTest < ApplicationSystemTestCase
   setup do
     @book = books(:one)
+
+    visit root_url
+    fill_in 'Eメール', with: 'alice@example.com'
+    fill_in 'パスワード', with: 'password'
+    click_on 'ログイン'
   end
 
   test 'visiting the index' do
     visit books_url
-    assert_selector 'h1', text: 'Books'
+    assert_selector 'h1', text: '本'
   end
 
   test 'creating a Book' do
     visit books_url
-    click_on 'New Book'
-
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Create Book'
-
-    assert_text 'Book was successfully created'
-    click_on 'Back'
+    click_on '新規作成'
+    fill_in 'タイトル', with: @book.title
+    fill_in 'メモ', with: @book.memo
+    fill_in '著者', with: @book.author
+    click_on '登録する'
+    assert_text '本が作成されました。'
   end
 
   test 'updating a Book' do
     visit books_url
-    click_on 'Edit', match: :first
-
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Update Book'
-
-    assert_text 'Book was successfully updated'
-    click_on 'Back'
+    click_on '編集', match: :prefer_exact
+    fill_in 'タイトル', with: @book.title
+    fill_in 'メモ', with: @book.memo
+    fill_in '著者', with: @book.author
+    click_on '更新する'
+    assert_text '本が更新されました。'
   end
 
   test 'destroying a Book' do
     visit books_url
     page.accept_confirm do
-      click_on 'Destroy', match: :first
+      click_on '削除', match: :first
     end
 
-    assert_text 'Book was successfully destroyed'
+    assert_text '本が削除されました。'
   end
 end
