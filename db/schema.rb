9fdf30fc8727f037da4769994a9b273a7a7feb57
damisumida <gjptmda628@gmail.com>
2022-12-24 16:36:32 +0900

    - 翻訳を追加 - 不要なカラムを追加していたマイグレーションファイルを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_21_035630) do
+ActiveRecord::Schema.define(version: 2022_12_21_035051) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -61,7 +61,6 @@ ActiveRecord::Schema.define(version: 2022_12_21_035630) do
     t.string "postal_code"
     t.string "address"
     t.text "self_introduction"
-    t.string "picture"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
