35f7336d0524820e735ad534534e279ee96c7273
damisumida <gjptmda628@gmail.com>
2022-12-24 14:46:22 +0900

    - user一覧、user詳細に画像表示を追加 - user編集にアバター項目を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   root to: 'books#index'
   resources :books
   resources :users, only: %i(index show)
