bea8241b2d30ed97d3c4e5532acb796359d06828
つるつる <84743939+Turu0512@users.noreply.github.com>
2023-01-27 11:09:39 +0900

    reportのviewを作成、日本語化

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -30,7 +30,7 @@ class ReportsController < ApplicationController
 
     respond_to do |format|
       if @report.save
-        format.html { redirect_to report_url(@report), notice: "Report was successfully created." }
+        format.html { redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Report.model_name.human) }
         format.json { render :show, status: :created, location: @report }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -43,7 +43,7 @@ class ReportsController < ApplicationController
   def update
     respond_to do |format|
       if @report.update(report_params)
-        format.html { redirect_to report_url(@report), notice: "Report was successfully updated." }
+        format.html { redirect_to report_url(@report), notice: t('controllers.common.notice_update', name: Report.model_name.human)}
         format.json { render :show, status: :ok, location: @report }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -58,7 +58,7 @@ class ReportsController < ApplicationController
       @report.destroy
 
       respond_to do |format|
-        format.html { redirect_to reports_url, notice: "Report was successfully destroyed." }
+        format.html { redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human) }
         format.json { head :no_content }
       end
     else
