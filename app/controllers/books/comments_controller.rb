0c202c134f328c9ee8b12aeeeeb9569f37b3a934
umizaru <y.wakita1T@gmail.com>
2023-02-03 13:09:51 +0900

    rubocop,erblint通過（commentのcontroller、不要なファイルを削除）

--- app/controllers/books/comments_controller.rb
+++ app/controllers/books/comments_controller.rb
@@ -1,9 +1,23 @@
 # frozen_string_literal: true
 
-class Books::CommentsController < CommentsController
+class Books::CommentsController < ApplicationController
   before_action :set_commentable, only: %i[create]
 
+  def create
+    @comment = @commentable.comments.build(comment_params)
+    @comment.user_id = current_user.id
+    if @comment.save
+      redirect_to @commentable, notice: t('controllers.common.notice_create', name: 'コメント')
+    else
+      render :new
+    end
+  end
+
   private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 
   def set_commentable
     @commentable = Book.find(params[:book_id])
