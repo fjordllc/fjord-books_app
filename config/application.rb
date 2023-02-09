e06f9b13719a013cd467052b5c6aaac89af5810d
つるつる <84743939+Turu0512@users.noreply.github.com>
2023-02-09 10:59:26 +0900

    投稿時間を表示と日本時間化

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module BooksApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.time_zone = 'Tokyo'
 
     # Configuration for the application, engines, and railties goes here.
     #
